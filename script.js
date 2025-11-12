f417468206dd8017fb01d21569d98191e448949d
Himanshu Singh <himanshusingh1k@gmail.com>
2025-11-12 17:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const gridItems = document.querySelectorAll(".grid-item");
+const inputItem = document.querySelector("#block_id");
+const inputColor = document.querySelector("#color_id");
 const changeBtn = document.querySelector("#change_button");
 const resetBtn = document.querySelector("#reset_button");
-const inputItem = document.querySelector("#block_id");
-const inputColor = document.querySelector("#colour_id");
+
 changeBtn.addEventListener("click", () => {
   gridItems.forEach((item) => (item.style.backgroundColor = "transparent"));
-  gridItems[inputItem.value - 1].style.backgroundColor = inputColor.value;
+  const index = Number(inputItem.value) - 1;
+  if (index >= 0 && index < gridItems.length)
+    gridItems[index].style.backgroundColor = inputColor.value;
 });
+
 resetBtn.addEventListener("click", () => {
-  gridItems[inputItem.value - 1].style.backgroundColor = "transparent";
+  const index = Number(inputItem.value) - 1;
+  if (index >= 0 && index < gridItems.length)
+    gridItems[index].style.backgroundColor = "transparent";
 });
